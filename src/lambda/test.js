9c9b4d057367d416d046bae0bd413c60d35f8306
mariliab <marilia.bognandi@gmail.com>
2020-08-25 14:03:49 +0200

    JSON stringofy added to data.name

--- src/lambda/test.js
+++ src/lambda/test.js
@@ -1,7 +1,7 @@
 exports.handler = (lambdaEvent, context, callback) => {
     const hasuraEvent = JSON.parse(lambdaEvent.body);
     const data = hasuraEvent.event.data.new;
-    callback.log(data.name + " was added!")
+    callback.log(JSON.stringify(data.name) + " was added!")
     console.log("DATA: " + JSON.stringify(data, null, 2))
     callback(null, {
         statusCode: 200,
