12d0d32d4106cbc076455d676de9230cc4c6588a
mariliab <marilia.bognandi@gmail.com>
2020-08-25 13:53:27 +0200

    JSON stringify to data

--- src/lambda/test.js
+++ src/lambda/test.js
@@ -1,7 +1,7 @@
 exports.handler = (lambdaEvent, context, callback) => {
     const hasuraEvent = JSON.parse(lambdaEvent.body);
     const data = hasuraEvent.event.data.new;
-    console.log("DATA: " + data)
+    console.log("DATA: " + JSON.stringify(data, null, 2))
     callback(null, {
         statusCode: 200,
         body: JSON.stringify({recievedData: data})
