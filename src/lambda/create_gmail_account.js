92aa029795852bfde8b599c61953c1cfb3500bf6
mariliab <marilia.bognandi@gmail.com>
2020-08-27 15:21:37 +0200

    callback added

--- src/lambda/create_gmail_account.js
+++ src/lambda/create_gmail_account.js
@@ -16,26 +16,26 @@ exports.handler = (lambdaEvent, context, callback) => {
     const data = hasuraEvent.event.data;
     console.log("DATA: " + JSON.stringify(data, null, 2))
 
+    const variables = data.new.gmailAddress
+    
     callback(null, {
         statusCode: 200,
         body: JSON.stringify({recievedData: data})
     });
-
-    // const variables = data.new
     
-    // return fetch("https://mt-onboarding-app-dev.herokuapp.com/v1/graphql", {
-    //     headers: {
-    //       "content-type": "application/json"
-    //     },
-    //     method: "POST",
-    //     body: JSON.stringify({ query: HASURA_OPERATION, variables })
-    //   })
-    //     .then(() => ({
-    //       statusCode: 200,
-    //       body: `Ok!`
-    //     }))
-    //     .catch(error => ({
-    //       statusCode: 422,
-    //       body: `Oops! Something went wrong. ${error}`
-    //     }));
+    return fetch("https://mt-onboarding-app-dev.herokuapp.com/v1/graphql", {
+        headers: {
+          "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({ query: HASURA_OPERATION, variables })
+      })
+        .then(() => ({
+          statusCode: 200,
+          body: `Ok!`
+        }))
+        .catch(error => ({
+          statusCode: 422,
+          body: `Oops! Something went wrong. ${error}`
+        }));
 }
